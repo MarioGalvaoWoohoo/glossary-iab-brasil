1f097787aa6feeaae9e458e635e843ff70d6627e
MarioGalvaoWoohoo <65731257+MarioGalvaoWoohoo@users.noreply.github.com>
2023-06-11 09:11:45 -0300

    ajuste na rota

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-12">
                             <div class="flex items-center">
-                                <a href="{{ route('search') }}">
+                                <a href="{{ route('web.search') }}">
                                     <div class="ml-4 text-lg leading-9 font-semibold text-gray-900 dark:text-white">IAB - Glossário</div>
                                 </a>
                             </div>
